a4ed7b1608279153b35d45e911d3290a841ae338
rng1 <naranjogonzalezrodrigo@gmail.com>
2023-09-26 17:08:32 +0200

    EJ2 gauss_kernel_1d

--- filters.py
+++ filters.py
@@ -1,7 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-MEDIAN_FILTER_SIZE = 3
+
+def gauss_kernel_1d(sigma):
+    """Calcula un kernel Gaussiano unidimensional con `sigma` dado
+
+    :param sigma: int
+        Parámetro σ de entrada.
+
+    :return kernel
+        El vector 1xN con el kernel de salida, teniendo en cuenta que:
+            - El centro x=0 de la Gaussiana está en la posición ⌊N/2⌋ + 1.
+            - N se calcula a partir de sigma como N = 2⌈3σ⌉ + 1
+    """
+    if sigma < 1:
+        raise ValueError(f"`sigma` must be greater than 0, got {sigma}")
+
+    n = 2 * round(3 * sigma) + 1
+    radius = np.floor(n / 2)
+    x = np.arange(-radius, radius + 1)
+    kernel = (1 / np.sqrt(2 * np.pi * sigma)) * np.exp(-x ** 2 / (2 * sigma ** 2))
+
+    return kernel
+
+
+def gaussian_filter(in_image, sigma):
+    """Realiza un suavizado Gaussiano bidimensional usando un filtro NxN de parámetro `sigma`,
+    donde N se calcula a partir de sigma como N = 2⌈3σ⌉ + 1.
+
+    :param in_image: array
+        Matriz MxN con la imagen de entrada.
+
+    :param sigma: int
+        Parámetro σ de entrada.
+
+    :return: out_image
+        Matriz MxN con la imagen de salida después de la aplicación del filtro.
+    """
+    in_image = np.asarray(in_image)
+    in_image = (in_image - np.min(in_image)) / (np.max(in_image) - np.min(in_image))
+    row, col = in_image.shape
+
+    kernel = np.asarray(gauss_kernel_1d(sigma))
+    print(kernel)
+
+    it1_image = np.zeros((row - 1, col - 1))
+    out_image = np.zeros((row - 1, col - 1))
+
+    for i in range(row - 1):
+        for j in range(col - 1):
+            window = in_image[i, j:j + len(kernel)]
+            it1_image[i, j] = window.sum() / kernel.sum()
+
+    it1_image = it1_image.transpose()
+
+    for i in range(row - 1):
+        for j in range(col - 1):
+            window = it1_image[i, j:j + len(kernel)]
+            out_image[i, j] = window.sum() / kernel.sum()
+
+    return out_image.transpose()
 
 
 def median_filter(in_image, filter_size):
@@ -20,34 +78,30 @@ def median_filter(in_image, filter_size):
     if filter_size < 1:
         raise ValueError(f"`filter_size` must be greater than 0, got {filter_size}")
 
-    indexer = filter_size // 2
-    out_image = np.zeros_like(in_image)
-    for i in range(len(in_image)):
-        for j in range(len(in_image[0])):
-            values = []
+    in_image = np.asarray(in_image)
+    in_image = (in_image - np.min(in_image)) / (np.max(in_image) - np.min(in_image))
 
-            for z in range(filter_size):
-                for k in range(filter_size):
-                    row_idx = i + z - indexer
-                    col_idx = j + k - indexer
+    row, col = in_image.shape
+    out_image = np.zeros((row - 1, col - 1))
 
-                    if 0 <= row_idx < len(in_image) and 0 <= col_idx < len(in_image[0]):
-                        values.append(in_image[row_idx][col_idx])
+    for i in range(row - 1):
+        for j in range(col - 1):
+            window = in_image[i:i + filter_size, j:j + filter_size]
+            out_image[i, j] = np.median(window)
 
-            values.sort()
-            out_image[i][j] = values[len(values) // 2]
     return out_image
 
 
 def get_output_and_show_filtered_images(in_image):
-    out_image_median = median_filter(in_image, MEDIAN_FILTER_SIZE)
+    out_image_gaussian = gaussian_filter(in_image, 1)
+    out_image_median = median_filter(in_image, filter_size=9)
 
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 5),
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(8, 5),
                              sharex=True, sharey=True)
     ax = axes.ravel()
 
-    titles = ['Original', 'Median filter']
-    imgs = [in_image, out_image_median]
+    titles = ['Original', 'Median filter', "Gaussian filter"]
+    imgs = [in_image, out_image_median, out_image_gaussian]
     for n in range(0, len(imgs)):
         ax[n].imshow(imgs[n], cmap=plt.cm.gray)
         ax[n].set_title(titles[n])
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import skimage
 import histograms
 import filters
 
-in_image = skimage.data.moon()[::10, ::10]
+in_image = skimage.data.coins()
 
-histograms.get_output_and_plot_histograms(in_image)
 filters.get_output_and_show_filtered_images(in_image)
